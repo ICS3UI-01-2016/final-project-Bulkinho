4d47f382d89cead55649b4270a947c061bd3a7e5
bulka4927 <bulka4927@HRH-IC0027338.wrdsb.ca>
2017-01-20 10:15:20 -0500

    notdonepoke

---
+++ src/PokemonGaem.java
@@ -0,0 +1,141 @@
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Random;
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author bulka4927
+ */
+
+
+public class PokemonGaem extends JComponent{
+
+    // Height and Width of our game
+    static final int WIDTH = 1000;
+    static final int HEIGHT = 800;
+ 
+    
+    // sets the framerate and delay for our game
+    // you just need to select an approproate framerate
+    long desiredFPS = 60;
+    long desiredTime = (1000)/desiredFPS;
+    //game variables
+    
+    
+    //background of game
+    BufferedImage pokebackground = loadImage ("route.png");
+    //enemy 
+    BufferedImage rattata = loadImage ("bad.png");
+    
+    
+    
+    // drawing of the game happens in here
+    // we use the Graphics object, g, to perform the drawing
+    // NOTE: This is already double buffered!(helps with framerate/speed)
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        // always clear the screen first!
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+        
+        
+        // GAME DRAWING GOES HERE 
+        g.drawImage (pokebackground, 0, 0,WIDTH, HEIGHT, null);
+        
+        g.setColor(Color.white);
+        
+        
+       
+       
+        
+        
+        
+        // GAME DRAWING ENDS HERE
+    }
+    
+    
+    // The main game loop
+    // In here is where all the logic for my game will go
+    public void Pokemon()
+    {
+        // Used to keep track of time used to draw and update the game
+        // This is used to limit the framerate later on
+        long startTime;
+        long deltaTime;
+        
+        
+        
+        // the main game loop section
+        // game will end if you set done = false;
+        boolean done = false; 
+        while(!done)
+        {
+            // determines when we started so we can keep a framerate
+            startTime = System.currentTimeMillis();
+            
+            // all your game rules and move is done in here
+            // GAME LOGIC STARTS HERE 
+            int x = 0;
+            int y = 0;
+            
+
+            // GAME LOGIC ENDS HERE 
+            
+            // update the drawing (calls paintComponent)
+            repaint();
+            
+            
+            
+            // SLOWS DOWN THE GAME BASED ON THE FRAMERATE ABOVE
+            // USING SOME SIMPLE MATH
+            deltaTime = System.currentTimeMillis() - startTime;
+            try
+            {
+               if(deltaTime > desiredTime)
+               {
+                   //took too much time, don't wait
+                   Thread.sleep(1);
+               }else{
+                  // sleep to make up the extra time
+                 Thread.sleep(desiredTime - deltaTime);
+               }
+            }catch(Exception e){};
+        }
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // creates a windows to show my game
+        JFrame frame = new JFrame("My Game");
+       
+        // creates an instance of my game
+        PokemonGaem game = new PokemonGaem();
+        // sets the size of my game
+        game.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        // adds the game to the window
+        frame.add(game);
+         
+        // sets some options and size of the window automatically
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        // shows the window to the user
+        frame.setVisible(true);
+        
+        // starts my game loop
+        game.Pokemon();
+    }
+}
